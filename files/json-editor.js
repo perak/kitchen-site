ff2c1f010e991d26f8f884d34c6b196badecae30
Petar Korponaić <petar.korponaic@gmail.com>
2014-11-22 16:14:51 +0100

    GUI: Added json editor

---
+++ files/json-editor.js
@@ -0,0 +1,76 @@
+Template.TEMPLATE_NAME.rendered = function() {
+	function resizeEditor() {
+		if(!$(".CodeMirror").length) {
+			return;
+		}
+		var viewHeight = $(window).height();
+		var footerHeight = $("#footer").outerHeight();
+		var codeTop = $(".CodeMirror").offset().top;
+
+		var availableHeight = viewHeight - footerHeight - codeTop;
+		if(availableHeight < 200) {
+			availableHeight = 200;
+		}
+
+		$(".CodeMirror").height(availableHeight);
+	}
+
+	resizeEditor();
+
+	$(window).on('resize', function() {
+		resizeEditor();
+	});
+}
+
+Template.TEMPLATE_NAME.helpers({
+	"editorOptions": function() {
+		return {
+			lineNumbers: true,
+			mode: "application/ld+json",
+			keyMap: "sublime",
+			theme: "blackboard"
+		}
+	},
+
+	"editorCode": function() {
+		var data = this.application.data;
+		objectRemoveMetadata(data);
+		return JSON.stringify(data, null, '\t');
+	}
+});
+
+Template.TEMPLATE_NAME.events({
+	"click .save-button": function(e, t) {
+		e.preventDefault();
+		var code = t.$("#json-editor").val();
+		var data = {};
+		try {
+			data = JSON.parse(code);
+		} catch(e) {
+			t.$("#app-editor-error-title").text("Error parsing JSON:");
+			t.$("#app-editor-error-message").text(e.message);
+			t.$(".app-editor-error").show();
+			return;
+		}
+		t.$(".app-editor-error").hide();
+
+		$(".save-button").button("loading");
+
+		extendWithMetadata(data, "root", this.metadata.data);
+
+		Applications.update({ _id: this.application._id }, { $set: { data: data }}, function(e) {
+			$(".save-button").button("reset");
+			if(e) {
+				t.$("#app-editor-error-title").text("Error saving data:");
+				t.$("#app-editor-error-message").text(e.message);
+				t.$(".app-editor-error").show();
+				return;
+			}
+		});
+	},
+
+	"click .app-editor-error-close": function(e, t) {
+		e.preventDefault();
+		$(".app-editor-error").hide();
+	}
+});
