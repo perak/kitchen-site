39f7a4c29e30c0759ad55e90a0f282662bf907be
euqip <benoit.pique@gmail.com>
2015-11-08 21:44:05 +0100

    original data view code

---
+++ files/dataview.js
@@ -0,0 +1,28 @@
+	"click #delete-button": function(e, t) {
+		e.preventDefault();
+		var me = this;
+		bootbox.dialog({
+			message: "Delete? Are you sure?",
+			title: "Delete",
+			animate: false,
+			buttons: {
+				success: {
+					label: "Yes",
+					className: "btn-success",
+					callback: function() {
+						Applications.remove({ _id: me._id });
+					}
+				},
+				danger: {
+					label: "No",
+					className: "btn-default"
+				}
+			}
+		});
+		return false;
+	},
+	"click #edit-button": function(e, t) {
+		e.preventDefault();
+		Router.go("applications.edit", {applicationId: this._id});
+		return false;
+	}
